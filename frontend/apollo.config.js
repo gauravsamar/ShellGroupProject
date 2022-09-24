0f57b2aa01d4a082e1a19e6fde46f0affe59dccc
gauravsamar <shaswat2814@gmail.com>
2022-09-24 19:01:18 +0530

    configured apollo-client in react

---
+++ frontend/apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    client : {
+        service : {
+            uri: "https://localhost:5001/graphql",
+            skipSSLValidation:true
+        }
+    }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {ApolloClient , ApolloProvider , HttpLink, InMemoryCache} from "@apollo/client";
+
+//Initializing apollo client
+const client  = new ApolloClient({
+  cache : new InMemoryCache(),
+  link: new HttpLink({
+    uri : "https://localhost:4000/graphql" //for consuming the graphql api
+  }),
+  credentials : "same-origin"
+})
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
