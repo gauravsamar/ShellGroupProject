6d6ed462ff5b55bb0fe005560f9d25a6923262ca
Bizla <Anika.Bizla@shell.com>
2022-09-28 12:17:27 +0530

    frontend changes

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import LoggedInNavbar from './LoggedInNavbar';
+export default function About(){
+
+return(
+    <div>
+<LoggedInNavbar/>
+<div style={{backgroundColor:'#C6EBC5', paddingLeft:'90px', paddingRight:'90px', paddingTop:'50px', paddingBottom:'50px'}}>
+<p style={{textAlign:'justify'}}>A measure of the total amount of carbon dioxide (CO2) and methane (CH4) 
+    emissions of a defined population, system or activity, considering all 
+    relevant sources, sinks and storage within the spatial and temporal boundary 
+    of the population, system or activity of interest. Calculated as carbon dioxide 
+    equivalent using the relevant 100-year global warming potential (GWP100).</p>
+
+
+<p style={{textAlign:'justify'}}>The concept and name of the carbon footprint was derived from the ecological footprint concept,[14] which was developed by William E. Rees and Mathis Wackernagel in the 1990s at the University of British Columbia. While carbon footprints are usually reported in tons of emissions (CO2-equivalent) per year, ecological footprints are usually reported in comparison to what the planet can renew. This assesses the number of "earths" that would be required if everyone on the planet consumed resources at the same level as the person calculating their ecological footprint. The carbon footprint is one part of the ecological footprint. Carbon footprints are more focused than ecological footprints since they merely measure emissions of gases that cause climate change into the atmosphere.</p>
+    </div>
+    </div>
+)
+
+
+}
+
